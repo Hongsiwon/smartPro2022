20a30238107939f465df07d6aeddf71308defd32
홍시원 <hsw010730@naver.com>
2022-03-10 16:15:56 +0900

    element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import dog from './dog.png';
 import './App.css';
 
+//JSX
+const testData = [
+  "제목 1 입니다."
+]
+
 function App() {
+  const h1Element = <h1>H1 제목 태그 입니당</h1>
+  const imgElement = <img src={dog} className = "App-logo" alt="logo" />;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={dog} className="App-logo" alt="logo" />
+
+        { h1Element }
+        { imgElement }
         <p>
           한글로 바꿔보세요. <code>src/App.js</code> and save to reload.
         </p>
